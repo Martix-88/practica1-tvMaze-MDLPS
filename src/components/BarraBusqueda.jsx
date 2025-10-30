e7240529bd2b0f07cb797ac6e843b1605e8411ee
Martix-88 <martadelapazsanchez@gmail.com>
2025-10-30 17:05:34 +0100

    Instalado react-hook-form. V1 de BarraBusqueda: formulario sencillo

--- src/components/BarraBusqueda.jsx
+++ src/components/BarraBusqueda.jsx
@@ -1,1 +1,24 @@
-export default function BarraBusqueda() {}
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+
+export default function BarraBusqueda({ onSearch }) {
+    // Inicilizamos el formulario
+    const { register, handleSubmit, reset } = useForm();
+
+    // Función que se ejecuta al enviar el formulario
+    function onSubmit(data) {
+        onSearch(data.search);
+        reset();
+    }
+
+    return (
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <input
+                {...register('search')}
+                type="text"
+                placeholder="Buscar serie..."
+            ></input>
+            <button type="submit">Search</button>
+        </form>
+    );
+}
