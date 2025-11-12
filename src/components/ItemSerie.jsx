3ae6e3171b1f74862dc83e1dfc9ca499caeb1701
Martix-88 <martadelapazsanchez@gmail.com>
2025-11-12 10:04:41 +0100

    FEAT: ItemSerie.jsx V1

--- src/components/ItemSerie.jsx
+++ src/components/ItemSerie.jsx
@@ -1,1 +1,51 @@
-export default function ItemSerie() {}
+export default function ItemSerie({
+    serie,
+    esFavorito,
+    onToggleFavorite,
+    onViewDetails,
+}) {
+    let tieneImagen = false;
+    let imagenAPI = '';
+
+    if (serie.image && serie.image.medium) {
+        tieneImagen = true;
+        imagenAPI = serie.image.medium;
+    }
+
+    return (
+        <div className="serie-item">
+            <div
+                className="serie-image-container"
+                onClick={() => onViewDetails(serie)}
+            >
+                {tieneImagen ? (
+                    // Muestra la imagen si está disponible
+                    <img
+                        src={imagenAPI}
+                        alt={`Poster de pelicula`}
+                        className="serie-image"
+                    />
+                ) : (
+                    // Muestra el placeholder de texto si no hay imagen
+                    <div className="no-image-placeholder">
+                        <span className="no-image-text">
+                            IMAGEN NO DISPONIBLE
+                        </span>
+                        <span className="no-image-name">{serie.name}</span>
+                    </div>
+                )}
+            </div>
+
+            <div className="serie-info">
+                <h3 className="serie-name">{serie.name}</h3>
+
+                <button
+                    onClick={() => onToggleFavorite(serie)}
+                    className={buttonClass}
+                >
+                    {esFavorito ? 'Quitar' : 'Favorito'}
+                </button>
+            </div>
+        </div>
+    );
+}
