56dcf45553fed3ade9d0323d90e4f212b4dc28c5
Martix-88 <martadelapazsanchez@gmail.com>
2025-11-15 14:28:59 +0100

    FEAT: ListaSeries.jsx V1 Creada el componente ListaSeries.jsx

--- src/components/ListaSeries.jsx
+++ src/components/ListaSeries.jsx
@@ -1,1 +1,37 @@
-export default function ListaSeries() {}
+import ItemSerie from './ItemSerie.jsx';
+
+export default function ListaSeries({
+    series,
+    favoritos,
+    onToggleFavorite,
+    onViewDetails,
+}) {
+    return (
+        <div className="seccion-series">
+            <h2 className="seccion-titulo">Resultados de la busqueda</h2>
+
+            {series.lenght === 0 ? (
+                <p>No hay resultados</p>
+            ) : (
+                <div className="show-grid">
+                    {series.map((item) => {
+                        const serie = item.show ? item.show : item;
+                        const esFavorito = favoritos.some(
+                            (f) => f.id === serie.id
+                        );
+
+                        return (
+                            <ItemSerie
+                                key={serie.id}
+                                serie={serie}
+                                esFavorito={esFavorito}
+                                onToggleFavorite={onToggleFavorite}
+                                onViewDetails={onViewDetails}
+                            />
+                        );
+                    })}
+                </div>
+            )}
+        </div>
+    );
+}
